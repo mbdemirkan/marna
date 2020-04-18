e7322fde76e12a0c26963b1f67b734d2394b8fcb
huseyin.kacar <huseyin.kacar@softtech.com.tr>
2020-04-18 15:04:37 +0300

    tabs added

--- app/src/main/java/com/softtech/marna/ui/search/SearchFragment.java
+++ app/src/main/java/com/softtech/marna/ui/search/SearchFragment.java
@@ -1,15 +1,15 @@
 package com.softtech.marna.ui.search;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.speech.RecognizerIntent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
+import android.widget.Button;
 
 import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
-import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
 
 import com.softtech.marna.R;
@@ -17,17 +17,41 @@ import com.softtech.marna.R;
 public class SearchFragment extends Fragment {
 
     private SearchViewModel searchViewModel;
+    private static final int REQUEST_CODE = 1234;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
         searchViewModel =
                 ViewModelProviders.of(this).get(SearchViewModel.class);
         View root = inflater.inflate(R.layout.fragment_search, container, false);
-        final TextView textView = root.findViewById(R.id.text_notifications);
-        searchViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
+        final Button search = (Button) root.findViewById(R.id.buttonSearch);
+        final Button speak = (Button) root.findViewById(R.id.buttonSpeak);
+        final Button barcode = (Button) root.findViewById(R.id.buttonBarcode);
+
+        search.setOnClickListener(new View.OnClickListener()
+        {
             @Override
-            public void onChanged(@Nullable String s) {
-                textView.setText(s);
+            public void onClick(View v)
+            {
+            }
+        });
+        speak.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+                Intent intent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
+                intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL, RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
+                intent.putExtra(RecognizerIntent.EXTRA_PROMPT, "Voice searching...");
+                startActivityForResult(intent, REQUEST_CODE);
+            }
+        });
+        barcode.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+
             }
         });
         return root;
