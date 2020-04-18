1f0c7166157e16d7210b265fcc5a9e7c50b40659
Bilal Demirkan <demirk@hotmail.com>
2020-04-18 20:51:46 +0300

    map

--- app/src/main/java/com/softtech/marna/ui/map/MapFragment.java
+++ app/src/main/java/com/softtech/marna/ui/map/MapFragment.java
@@ -1,13 +1,19 @@
 package com.softtech.marna.ui.map;
 
 import android.Manifest;
+import android.content.Context;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.Toast;
 
 import androidx.fragment.app.Fragment;
 
+import com.google.android.gms.common.GooglePlayServicesNotAvailableException;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.MapView;
@@ -22,6 +28,8 @@ public class MapFragment extends Fragment {
 
     MapView mMapView;
     private GoogleMap googleMap;
+    public WebView mWebView;
+
     private static final String[] INITIAL_PERMS={
             Manifest.permission.ACCESS_FINE_LOCATION
     };
@@ -38,8 +46,9 @@ public class MapFragment extends Fragment {
 
         mMapView.onResume(); // needed to get the map to display immediately
 
+        // Needs to call MapsInitializer before doing any CameraUpdateFactory calls
         try {
-            MapsInitializer.initialize(getActivity().getApplicationContext());
+            MapsInitializer.initialize(this.getActivity());
         } catch (Exception e) {
             e.printStackTrace();
         }
@@ -50,6 +59,7 @@ public class MapFragment extends Fragment {
                 googleMap = mMap;
 
                 // For showing a move to my location button
+                googleMap.getUiSettings().setMyLocationButtonEnabled(false);
                 googleMap.setMyLocationEnabled(true);
 
                 // For dropping a marker at a point on the Map
@@ -62,30 +72,41 @@ public class MapFragment extends Fragment {
             }
         });
 
+        mWebView = (WebView) rootView.findViewById(R.id.webView);
+        mWebView.setWebViewClient(new WebViewClient());
+
+        // Enable Javascript
+        WebSettings webSettings = mWebView.getSettings();
+
+        webSettings.setJavaScriptEnabled(true);
+
+        // Force links and redirects to open in the WebView instead of in a browser
+        mWebView.loadUrl("https://www.google.com.tr/maps/search/Marketler/@39.9317221,32.8460563,16z/data=!3m1!4b1");
+
         return rootView;
     }
 
     @Override
     public void onResume() {
         super.onResume();
-        mMapView.onResume();
+//        mMapView.onResume();
     }
 
     @Override
     public void onPause() {
         super.onPause();
-        mMapView.onPause();
+//        mMapView.onPause();
     }
 
     @Override
     public void onDestroy() {
         super.onDestroy();
-        mMapView.onDestroy();
+//        mMapView.onDestroy();
     }
 
     @Override
     public void onLowMemory() {
         super.onLowMemory();
-        mMapView.onLowMemory();
+//        mMapView.onLowMemory();
     }
 }
