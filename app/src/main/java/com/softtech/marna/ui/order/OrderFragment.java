cbdd99b46d48d9a219436f62660d4a1bda5c4034
Bilal Demirkan <demirk@hotmail.com>
2020-04-19 14:53:39 +0300

    siparliş lite

--- app/src/main/java/com/softtech/marna/ui/order/OrderFragment.java
+++ app/src/main/java/com/softtech/marna/ui/order/OrderFragment.java
@@ -4,6 +4,8 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -22,6 +24,17 @@ public class OrderFragment extends Fragment {
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_siparis_durumu, container, false);
+
+        String[] ulkeler =
+                {"15 numaralı sipariş: Hazırlanıyor", "16 numaralı sipariş: Göndrrildi"};
+//(A) adımı
+        ListView listemiz=(ListView) view.findViewById(R.id.orderStatusListView);
+
+        //(B) adımı
+        ArrayAdapter<String> veriAdaptoru=new ArrayAdapter<String>(this.getActivity(), android.R.layout.simple_list_item_1, android.R.id.text1, ulkeler);
+
+        //(C) adımı
+        listemiz.setAdapter(veriAdaptoru);
 
         return view;
     }
